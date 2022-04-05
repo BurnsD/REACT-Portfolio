a0c5a09c4e7ccb70bda5510e3393d4fc3179653e
BurnsD <Dustinburnsactech@gmail.com>
2022-04-05 15:43:17 -0500

    work component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -9,11 +9,11 @@ import ReactImg from '../assets/react.png';
 
 const Skills = () => {
   return (
-    <div name='skills' className='bg-[#0a192f] text-gray-300 py-4'>
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300 py-4'>
         {/* Contianer */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p className='text-4xl font-bold inline border-b-4 border-yellow-300'>Experience</p>
+                <p className='text-4xl font-bold inline border-b-4 border-yellow-300'>Skills</p>
                 <p className='py-4'>// These are the technologies I work with</p>
             </div>
 
