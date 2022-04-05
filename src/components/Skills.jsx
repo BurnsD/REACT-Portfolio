c9ff5f32a9d81417653b10c9f3e2e63bd1236705
BurnsD <Dustinburnsactech@gmail.com>
2022-04-05 15:31:41 -0500

    skills section added with images

---
+++ src/components/Skills.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import Mongo from '../assets/mongo.png';
+import Tailwind from '../assets/tailwind.png';
+import ReactImg from '../assets/react.png';
+
+const Skills = () => {
+  return (
+    <div name='skills' className='bg-[#0a192f] text-gray-300 py-4'>
+        {/* Contianer */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div>
+                <p className='text-4xl font-bold inline border-b-4 border-yellow-300'>Experience</p>
+                <p className='py-4'>// These are the technologies I work with</p>
+            </div>
+
+            <div className='w-fulll grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-6'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
+                    <img className='w-20 mx-auto' src={CSS} alt="icon" />
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
+                    <img className='w-20 mx-auto' src={JavaScript} alt="icon" />
+                    <p className='my-4'>JavaScript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
+                    <img className='w-20 mx-auto' src={Mongo} alt="icon" />
+                    <p className='my-4'>MongoDB</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
+                    <img className='w-20 mx-auto' src={Tailwind} alt="icon" />
+                    <p className='my-4'>Tailwind</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
+                    <img className='w-20 mx-auto' src={ReactImg} alt="icon" />
+                    <p className='my-4'>REACT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-300'>
+                    <img className='w-20 mx-auto' src={HTML} alt="icon" />
+                    <p className='my-4'>HTML</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
