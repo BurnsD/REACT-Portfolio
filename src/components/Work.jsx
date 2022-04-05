ee6f8d155bf81548fe42ab87646bba2c32a0918f
BurnsD <Dustinburnsactech@gmail.com>
2022-04-05 15:43:41 -0500

    work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Work = () => {
   return (
-    <div>Work</div>
+    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        {/* Contianer */}
+        <div>
+            <div>
+                <p>Work</p>
+                <p>// Some of my recent projects</p>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <span>
+
+                        </span>
+                        <div>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                            <a href="/">
+                                <button></button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
   )
 }
 
