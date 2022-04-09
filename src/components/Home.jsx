29f4db58858754e5c9c56731a166c686b8186c3a
BurnsD <Dustinburnsactech@gmail.com>
2022-04-09 13:18:06 -0500

    single typewriter working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,27 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
 import { Link } from 'react-scroll';
+import { useState, useEffect } from 'react';
+
+const description = 'Full Stack Developer'
 
 const Home = () => {
+    const [typedDescription, setTypedDescription] = useState('')
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setTypedDescription(description.slice(0, typedDescription.length + 1))
+        }, 300)
+
+        return () => clearTimeout(timeout)
+    }, [typedDescription])
+
     return (
         <div name='home' className='w-full h-screen bg-[#0a192f]'>
             {/* Container */}
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
                 <p className='text-yellow-300'>Howdy, my name is</p>
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Dustin Burns</h1>
-                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Full Stack Developer</h2>
+                <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0] blinking-cursor'>{typedDescription}</h2>
                 <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil delectus, est
                     odit culpa pariatur aperiam impedit a atque animi, nobis laboriosam cupiditate
                     consequatur enim, quod aliquam. At ducimus sed laudantium!</p>
