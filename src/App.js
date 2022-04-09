13742ae591c59158f5f919e9f35752b53a66089a
BurnsD <Dustinburnsactech@gmail.com>
2022-04-09 12:44:30 -0500

    checkout to new branch /typewriter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Home from "./components/Home";
 import { Navbar } from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
