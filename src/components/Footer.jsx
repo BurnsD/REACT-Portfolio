818a4c2e7ab4584ff98c2cbc8e2ee037f42cda27
BurnsD <Dustinburnsactech@gmail.com>
2022-04-09 20:13:54 -0500

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from 'react-icons/fa'
+
+const Footer = () => {
+    return (
+
+        <footer className="flex flex-col items-center justify-between px-6 py-4 bg-[#0a192f] dark:bg-gray-800 sm:flex-row">
+
+
+            <p className="py-2 text-gray-800 dark:text-white sm:py-0">All rights reserved</p>
+
+            <div className="flex -mx-2 ">
+                <a className='flex justify-between items-center w-full  text-gray-300'
+                    href="https://www.linkedin.com/in/dustin-burns-b176b1226/">
+                    <FaLinkedin size={30} className='bg-blue-600'/>
+                </a>
+           
+                <a className='flex justify-between items-center pl-5 w-full  text-gray-300'
+                    href="https://github.com/BurnsD">
+                    <FaGithub size={30} className='bg-gray-600' />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -107,7 +107,7 @@ export const Navbar = () => {
                     </li>
                     <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-[#6fc2b0]'>
                         <a className='flex justify-between items-center w-full text-gray-300'
-                            href="/">
+                            href="mailto:Contact@DustinBurnsDev.com">
                             Email <HiOutlineMail size={30} />
                         </a>
                     </li>
