470ecb82751526ff2c901ed7edea31746d3b1ef2
BurnsD <Dustinburnsactech@gmail.com>
2022-04-10 12:27:50 -0500

    updated footer icon border and added twitter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaGithub, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 const Footer = () => {
     return (
@@ -8,11 +8,15 @@ const Footer = () => {
             <div className="flex -mx-2 ">
                 <a className='flex justify-between items-center w-full  text-gray-300'
                     href="https://www.linkedin.com/in/dustin-burns-b176b1226/">
-                    <FaLinkedin size={30} className='bg-blue-600'/>
+                    <FaLinkedin size={30} className='bg-blue-600 rounded-md'/>
                 </a>
                 <a className='flex justify-between items-center pl-5 w-full  text-gray-300'
                     href="https://github.com/BurnsD">
-                    <FaGithub size={30} className='bg-gray-600' />
+                    <FaGithub size={30} className='bg-gray-600 rounded-md' />
+                </a>
+                <a className='flex justify-between items-center pl-5 w-full  text-gray-300'
+                    href="https://twitter.com/DustinBurnsDev">
+                    <FaTwitter size={30} className='bg-blue-600 rounded-md ' />
                 </a>
             </div>
         </footer>
